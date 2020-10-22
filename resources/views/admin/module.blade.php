82b95797dfec838c631e6509b5ea42ca83be185e
Nail-cathcer <nikita-mokin@mail.ru>
2020-10-23 03:28:02 +0500

    kwjecwj

--- resources/views/admin/module.blade.php
+++ resources/views/admin/module.blade.php
@@ -1,8 +1,19 @@
-@extends('admin.layouts_admin.main_layout_admin')
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <!-- CSRF Token --> <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+ <link rel="stylesheet" href="{{ asset('vendor/file-manager/css/file-manager.css') }}">
 
-@section('content')
-<li><a href="{{route('admin')}}">Вернуться в админку</a></li>
+<script src="{{ asset('vendor/file-manager/js/file-manager.js') }}"></script>
+    
 
+</head>
+<body>
+
+<li><a href="{{route('admin')}}">Главная</a></li>
 <div style="height: 600px;">
     <div id="fm"></div>
 </div>
@@ -10,5 +21,6 @@
 </div>
 <script src="{{ asset('vendor/file-manager/js/file-manager.js') }}"></script>
 
-@endsection
+</body>
+</html>
 
