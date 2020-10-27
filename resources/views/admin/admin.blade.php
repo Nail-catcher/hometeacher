0a23394a828ac66a9a9640e0783363010e4ba3b8
Nail-cathcer <nikita-mokin@mail.ru>
2020-10-27 07:44:15 +0500

    Initial commit.

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -11,13 +11,13 @@ var popCanvas = document.getElementById("popChart").getContext("2d");
 var barChart = new Chart(popCanvas, {
   type: 'bar',
   data: {
-    labels: ["Зарегистрировались", "Скачали пробник", "Оформили подписку"],
+    labels: ["Зарегистрировались",  "Оформили подписку"],
     datasets: [{
       label: 'Статистика',
       data: [{{$Users}},0, {{$Subscriptions}},0],
       backgroundColor: [
         'rgba(255, 99, 132, 0.6)',
-        'rgba(54, 162, 235, 0.6)',
+        
         'rgba(255, 206, 86, 0.6)',
         
       ]
